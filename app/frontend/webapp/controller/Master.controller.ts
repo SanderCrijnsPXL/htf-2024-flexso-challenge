e8a5cdc0ab93627efe43256b1eee70a260377b53
SanderCrijnsPXL <sander.crijns@student.pxl.be>
2024-11-19 14:39:23 +0100

    fix drake

--- app/frontend/webapp/controller/Master.controller.ts
+++ app/frontend/webapp/controller/Master.controller.ts
@@ -1,7 +1,9 @@
 import Controller from "sap/ui/core/mvc/Controller";
-import UIComponent from "sap/ui/core/UIComponent";
 import formatter from "../model/formatter";
-import Event from "sap/ui/base/Event";
+import UIComponent from "sap/ui/core/UIComponent";
+import GenericTile from "sap/m/GenericTile";
+import { LayoutType } from "sap/f/library";
+import UI5Event from "sap/ui/base/Event";
 
 
 /**
@@ -12,4 +14,17 @@ export default class Master extends Controller {
 
   /*eslint-disable @typescript-eslint/no-empty-function*/
   public onInit(): void {}
+
+  private pressPlanet(event: UI5Event) {
+    (this.getOwnerComponent() as UIComponent).getRouter().navTo("Detail", {
+      GalaxyId: (event.getSource() as GenericTile)
+        ?.getBindingContext()
+        ?.getProperty("ID") as number,
+      layout: LayoutType.TwoColumnsBeginExpanded,
+    });
+  }
+
+  private onSearch(event: UI5Event) {}
+
+  private clearFilters(event: UI5Event) {}
 }
